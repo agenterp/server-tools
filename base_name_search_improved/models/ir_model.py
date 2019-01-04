b8bec5bc9c9e927fdc975063169bc9db195426a7
notze-nb2 <georg.notter@agent-erp.de>
2019-01-04 16:39:46 +0100

    fixed error in base_name search wrong check

--- base_name_search_improved/models/ir_model.py
+++ base_name_search_improved/models/ir_model.py
@@ -19,7 +19,7 @@ def _get_rec_names(self):
 
 def _extend_name_results(self, domain, results, limit):
     result_count = len(results)
-    if result_count < limit:
+    if limit and result_count < limit:
         domain += [('id', 'not in', [x[0] for x in results])]
         recs = self.search(domain, limit=limit - result_count)
         results.extend(recs.name_get())
